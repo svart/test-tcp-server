67346b924723a11ff312fa96b37d2dc8fe1706a0
Dmitry Shinkaruk <dimashink@gmail.com>
2024-11-01 22:57:08 +0300

    Remove unnecessary logs

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,18 @@ async fn server_connection_handler(mut sock: TcpStream) {
         match sock.read_exact(&mut send_buf[..HEADER_SIZE]).await {
             Ok(0) | Err(_) => return,
             Ok(_) => {
-                let req_id = u64::from_be_bytes(send_buf[..ONE_HEADER_FIELD].try_into().expect("request should be ok"));
+                let _req_id = u64::from_be_bytes(send_buf[..ONE_HEADER_FIELD].try_into().expect("request should be ok"));
                 let mut left_to_send = u64::from_be_bytes(send_buf[ONE_HEADER_FIELD..HEADER_SIZE].try_into().expect("size should be ok")) as usize;
 
-                println!("Received request {req_id} to send {left_to_send} bytes");
+                // println!("Received request {req_id} to send {left_to_send} bytes");
 
                 // We need to send header at least
                 left_to_send = max(left_to_send, HEADER_SIZE);
 
                 while left_to_send > 0 {
                     let fun = if left_to_send < SEND_BUFFER_SIZE {
-                        println!("sending {left_to_send} bytes");
                         sock.write_all(&send_buf[..left_to_send])
                     } else {
-                        println!("sending {SEND_BUFFER_SIZE} bytes");
                         sock.write_all(&send_buf)
                     };
 
@@ -38,7 +36,6 @@ async fn server_connection_handler(mut sock: TcpStream) {
                     }
                     left_to_send -= min(left_to_send, SEND_BUFFER_SIZE);
                 }
-                println!("sending done");
             }
         }
     }
@@ -53,7 +50,8 @@ async fn main() {
 
     loop {
         match listen_sock.accept().await {
-            Ok((socket, _)) => {
+            Ok((socket, addr)) => {
+                println!("Got connection from {addr}");
                 tokio::spawn(server_connection_handler(socket));
             }
             Err(e) => println!("Accepting connection failed: {e}"),
